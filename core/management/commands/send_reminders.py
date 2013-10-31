c63fc78ef2c416c449db5f887b965b77208ec7f9
Tim Saylor <tim.saylor@gmail.com>
2013-10-31 08:28:18 -0500

    reminder models and sending command

---
+++ core/management/commands/send_reminders.py
@@ -0,0 +1,26 @@
+''' Finds all morsels due for a reminder and sends them. '''
+from django.core.management.base import NoArgsCommand, CommandError
+from core import models
+from datetime import datetime
+
+class Command(NoArgsCommand):
+    help = 'Finds all morsels due for a reminder and sends them.'
+
+    def handle_noargs(self, **options):
+        schedule = models.ReminderDelay.objects.values_list('days', flat=True)
+        now = datetime.now()
+        for morsel in models.Morsel.objects.all():
+            days = (now - morsel.created()).days
+            if days in schedule and not models.Reminder.objects.filter(
+                morsel=morsel, delay__days=days
+            ).exists():
+                send_mail(
+                    morsel.title, morsel.content, settings.DEFAULT_FROM_EMAIL,
+                    [morsel.user.email_address], fail_silently=False
+                )
+                models.Reminder.objects.create(
+                    morsel=morsel,
+                    delay=models.ReminderDelay.objects.filter(days=days)
+                )
+            
+
--- core/models.py
+++ core/models.py
@@ -19,6 +19,9 @@ class Morsel(BaseModel):
     def get_absolute_url(self):
         return reverse('morsel_detail', kwargs={'pk': self.id})
 
+    def __unicode__(self):
+        return "%s-%s" % (self.user, self.title)
+
 
 class MorselForm(forms.ModelForm):
     user = forms.ModelChoiceField(
@@ -27,3 +30,21 @@ class MorselForm(forms.ModelForm):
 
     class Meta:
         model = Morsel
+
+
+class ReminderDelay(models.Model):
+    days = models.IntegerField()
+    recurring = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['days']
+
+    def __unicode__(self):
+        return "%s%d days" % ('every ' if self.recurring else '', self.days)
+
+class Reminder(BaseModel):
+    morsel = models.ForeignKey(Morsel)
+    delay = models.ForeignKey(ReminderDelay)
+
+    def __unicode__(self):
+        return "%s after %d days" % (self.morsel, self.delay.days)
