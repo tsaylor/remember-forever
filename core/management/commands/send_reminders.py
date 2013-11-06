2b3c6e71bfb0be99f356b98a09bcf7f13993dca2
Tim Saylor <tim.saylor@gmail.com>
2013-11-05 23:55:23 -0600

    got send_reminders sending live emails

--- core/management/commands/send_reminders.py
+++ core/management/commands/send_reminders.py
@@ -1,7 +1,10 @@
 ''' Finds all morsels due for a reminder and sends them. '''
 from django.core.management.base import NoArgsCommand, CommandError
+from django.core.mail import send_mail
+from django.conf import settings
 from core import models
 from datetime import datetime
+
 
 class Command(NoArgsCommand):
     help = 'Finds all morsels due for a reminder and sends them.'
@@ -10,13 +13,13 @@ class Command(NoArgsCommand):
         schedule = models.ReminderDelay.objects.values_list('days', flat=True)
         now = datetime.now()
         for morsel in models.Morsel.objects.all():
-            days = (now - morsel.created()).days
+            days = (now - morsel.created.replace(tzinfo=None)).days
             if days in schedule and not models.Reminder.objects.filter(
                 morsel=morsel, delay__days=days
             ).exists():
                 send_mail(
                     morsel.title, morsel.content, settings.DEFAULT_FROM_EMAIL,
-                    [morsel.user.email_address], fail_silently=False
+                    [morsel.user.email], fail_silently=False
                 )
                 models.Reminder.objects.create(
                     morsel=morsel,
